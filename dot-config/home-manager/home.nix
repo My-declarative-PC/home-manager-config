526130b9b3a51b4ad5109c28a7c45b57d2801588
Sitnikov Timofey <tima.sitnikov@mail.ru>
2024-07-21 17:58:08 +0000

    packet dlya lechshey navigacii po direktoriyam

--- dot-config/home-manager/home.nix
+++ dot-config/home-manager/home.nix
@@ -28,6 +28,7 @@
     pkgs.lazygit
     pkgs.starship
     pkgs.zellij
+    pkgs.zoxide
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
